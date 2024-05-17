3807df25e2a0810738de0f97d966f256dda79f88
natancolen <natancolen@gmail.com>
2024-05-17 10:26:24 -0300

    Criando funções

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-print("""
+import os
+
+def exibir_nome_programa():
+    print("""
       
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -7,10 +10,35 @@ print("""
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░      
       """)
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair \n')
 
-opcao_escohilhida = input('Digite uma opção: ')
-print(f'Você escolheu a opção: {opcao_escohilhida}!')
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair \n')
+
+def finalizar_app():
+    os.system('clear')
+    print('Encerrando o programa')
+
+def escolher_opcao():
+    opcao_escohilhida = int(input('Digite uma opção: '))
+    print(f'Você escolheu a opção: {opcao_escohilhida}!')
+
+    if opcao_escohilhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escohilhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escohilhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
+
